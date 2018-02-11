3d64ac654778b684a42c74927b2fb8f5a069113d
Jonathan Park <park6134@pacificu.edu>
2018-02-10 18:02:33 -0800

    Comments updated. Still simple testing code.

--- CS594rcp_client.c
+++ CS594rcp_client.c
@@ -1,10 +1,11 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-//
-//
-//
-//
-////////////////////////////////////////////////////////////////////////////////
+/******************************************************************************/
+/*      								      */
+/* Author:      Jonathan M. Park                                              */
+/* Date:        02/10/2018                                                    */
+/* Descruption: A                                                             */
+/*									      */
+/*								 	      */
+/******************************************************************************/
 
 #include <stdio.h>      /* for standard i/o  */
 #include <sys/types.h>  /* for sizes  */
@@ -21,45 +22,45 @@ void err(char *);
 int
 main(int argc, char **argv)
 {
-  int port;                     /* int of the port_no as specificed by the user  */
-  int rc;                       /* return code for error handling  */
-  int serv_sock;                /* socket identifier for the user specified server   */
+  int port;                     /* The port_no as specificed by the user.  */
+  int rc;                       /* Return code for error handling.  */
+  int serv_sock;                /* Socket descriptorr for the server.  */
   int serv_addr_size;           /* size of server address for recvfrom()  */
-  char *serv_ip;                /* IP address of the server specified by the user  */
-  char buf[BUFFERSIZE];         /* temporary buffer for message testing  */
-  char res[BUFFERSIZE];         /* temporary buffer for response testing  */
-  struct sockaddr_in serv_addr; /* socket API defined form for addresses  */
+  char *serv_ip;                /* IP address of the server.  */
+  char buf[BUFFERSIZE];         /* Temporary buffer for message testing.  */
+  char res[BUFFERSIZE];         /* Temporary buffer for response testing.  */
+  struct sockaddr_in serv_addr; /* Socket API defined form for addresses  */
 
   /* argv[1] is the the IP address of the server.
      argv[2] is the port number of the server.
      argv[3] is the file path for the desired file.  */
   if(argc < 4 || argc > 5)
-    err("Invalid Usage: should be ./CS594rcp_client IP_addr port_no file_path.\n");
+    err("Usage: should be ./CS594rcp_client IP_addr port_no file_path.\n");
 
   serv_ip = argv[1];
   port = atoi(argv[2]);
 
-  /* Create UDP socket  */
+  /* Create a  UDP socket.  */
   serv_sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
   if(serv_sock < 0)
     err("Socket not created.\n");
 
-  /* memset to zero serv_addr struct and then fields are filled with 
+  /* bzero() is used to zero serv_addr struct and then fields are filled with 
      appropriate values  */
-  memset(&serv_addr, 0, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET; /* Interner Domain  */
-  serv_addr.sin_port = htons(port); /* Server Port  */
-  /* htons() used to solve endianness incompatibility across machines */
-  serv_addr.sin_addr.s_addr = inet_addr(serv_ip); /* Server's Address  */
+  bzero(&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET; /* Interner domain.  */
+  serv_addr.sin_port = htons(port); /* Server port.  */
+  /* htons() used to solve endianness incompatibility across machines.  */
+  serv_addr.sin_addr.s_addr = inet_addr(serv_ip); /* Server's address.  */
 
-  strcpy(buf, "Hello.\n"); /* buffer used for testing  */
-  memset(&res, 0, sizeof(res)); /* fill response buffer with zero  */
+  strcpy(buf, "Hello.\n"); /* Simple buffer used for testing.  */
+  memset(&res, 0, sizeof(res)); /* Fill the response buffer with zeros.  */
 
   serv_addr_size = sizeof(serv_addr);
 
-  /* send buffer message to server  */
-  rc = sendto(serv_sock, buf, (strlen(buf) + 1), 0, (struct sockaddr *) &serv_addr,
-     sizeof(serv_addr));
+  /* Send the buffer message to the server.  */
+  rc = sendto(serv_sock, buf, (strlen(buf) + 1), 0, 
+		  (struct sockaddr *) &serv_addr, sizeof(serv_addr));
   if(rc < 0)
     err("sendto() failed.\n");
 

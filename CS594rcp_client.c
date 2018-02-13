6849244e0f2f5a4f9013de0189bd7d88ddb5b411
Jonathan Park <park6134@pacificu.edu>
2018-02-13 09:31:08 -0800

    Able to send small text files.

--- CS594rcp_client.c
+++ CS594rcp_client.c
@@ -14,9 +14,10 @@
 #include <stdlib.h>     /* for atoi()  */
 #include <string.h>     /* for memset() and strcpy()  */
 #include <unistd.h>     /* for close()  */
+#include "packet.h"     
 
-#define BUFFERSIZE 32
-
+void cli_handshake(int, struct sockaddr *, socklen_t, char *);
+void write_file(char *, char *);
 void err(char *);
 
 int
@@ -27,9 +28,11 @@ main(int argc, char **argv)
   int serv_sock;                /* Socket descriptorr for the server.  */
   int serv_addr_size;           /* size of server address for recvfrom()  */
   char *serv_ip;                /* IP address of the server.  */
-  char buf[BUFFERSIZE];         /* Temporary buffer for message testing.  */
-  char res[BUFFERSIZE];         /* Temporary buffer for response testing.  */
+  /* char buf[MAX_DGRAM_LEN];      * Temporary buffer for message testing.  */
+  char res[MAX_DGRAM_LEN];      /* Temporary buffer for response testing.  */
+  char ack_msg[MAX_DGRAM_LEN];
   struct sockaddr_in serv_addr; /* Socket API defined form for addresses  */
+  char last_seq = '1';
 
   /* argv[1] is the the IP address of the server.
      argv[2] is the port number of the server.
@@ -53,26 +56,61 @@ main(int argc, char **argv)
   /* htons() used to solve endianness incompatibility across machines.  */
   serv_addr.sin_addr.s_addr = inet_addr(serv_ip); /* Server's address.  */
 
-  strcpy(buf, "Hello.\n"); /* Simple buffer used for testing.  */
   memset(&res, 0, sizeof(res)); /* Fill the response buffer with zeros.  */
 
   serv_addr_size = sizeof(serv_addr);
 
-  /* Send the buffer message to the server.  */
-  rc = sendto(serv_sock, buf, (strlen(buf) + 1), 0, 
-		  (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-  if(rc < 0)
-    err("sendto() failed.\n");
+  cli_handshake(serv_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr), argv[3]);  
 
-  /* receive message from server  */
-  rc = recvfrom(serv_sock, res, sizeof(res), 0, (struct sockaddr *) &serv_addr,
+  do{
+    bzero(&ack_msg, sizeof(ack_msg));
+    ack_msg[0] = dg_type_arry[DG_ACK];
+    memset(&res, 0, sizeof(res)); /* Fill the response buffer with zeros.  */
+    /* receive message from server  */
+    rc = recvfrom(serv_sock, res, MAX_DGRAM_LEN + 1, 0, (struct sockaddr *) &serv_addr,
                 (socklen_t *) &serv_addr_size);
-  if(rc < 0)
-    err("recvfrom() failed.\n");
 
-  printf("Message Received from server: %s", res);
+    printf("%s\n", res);
+    if(rc < 0){
+      err("recvfrom() failed.\n");
+      break;
+    }
+    if(res[0] == dg_type_arry[DG_CLOSE])
+      break;
+    /* else if(res[1] == last_seq){ //if duplicate
+      ack_msg[1] = last_seq;
+      rc = sendto(serv_sock, ack_msg, MAX_DGRAM_LEN, 0, (struct sockaddr *) &serv_addr,
+	      	  (socklen_t) serv_addr_size);
+    } */
+    else{
+      
+      /* testing.  */
+      FILE *f;
 
-  close(serv_sock); /* Close server connection  */
+      f = fopen("myfile.txt", "w"); /* removed wb for testing */
+      if(!f)
+	err("File could not be made.\n");
+
+      printf("%s\n", res);
+      fwrite(res + 3, 1, rc, f);
+
+      ack_msg[1] = res[1];
+
+      last_seq = res[1];
+
+      rc = sendto(serv_sock, ack_msg, MAX_DGRAM_LEN, 0, (struct sockaddr *) &serv_addr,
+		  (socklen_t) serv_addr_size);
+
+      /*   rc = sendto(serv_sock, ack_msg, MAX_DGRAM_LEN, 0, (struct sockaddr *) & serv_addr,
+	  (socklen_t) serv_addr_size); */
+      // write_file("myfile.txt", res);
+      if(rc < 0)
+        err("sendto() failed.\n");
+    }
+  } while(res[0] != dg_type_arry[DG_CLOSE]);
+
+  // if(res[0] == dg_type_arry[DG_CLOSE])
+      close(serv_sock); /* Close server connection  */
 
   return 0;
 }
@@ -85,5 +123,48 @@ err(char *msg)
 {
   perror(msg);
   exit(0);
+}
+
+void
+write_file(char *name, char *buf)
+{
+    FILE *f;
+    
+    f = fopen(name, "wb");
+    if(!f)
+	err("File could not be made.\n");
+
+    printf("%lu\n", sizeof(buf));
+    fwrite(buf, 1, sizeof(buf), f);
+
+    fclose(f);
+}
+
+void
+cli_handshake(int sockfd, struct sockaddr * paddr, socklen_t servlen, char *pathname)
+{
+    int rc;
+    char msg1[MAX_DGRAM_LEN];
+    char msg2[MAX_DGRAM_LEN];
+    char msg3[MAX_DGRAM_LEN];
+    socklen_t len = servlen;
+
+    msg1[0] = dg_type_arry[DG_HELLO];
+    rc = sendto(sockfd, msg1, MAX_DGRAM_LEN, 0, paddr, len);
+    if(rc < 0)
+	err("Unable to start handshake.\n");
+    rc = recvfrom(sockfd, msg2, MAX_DGRAM_LEN, 0, paddr, &len);
+    if(rc < 0)
+	err("Unable to establish handshake.\n");
+    do{
+	msg3[0] = dg_type_arry[DG_DATA];
+	msg3[1] = '0';
+	msg3[2] = '1';
+	strncpy(msg3 + 3, pathname, strlen(pathname));
+
+	rc = sendto(sockfd, msg3, MAX_DGRAM_LEN, 0, paddr, len);
+	if(rc < 0)
+	    printf("Unable to send pathname to server. Will try again.\n");
+    } while(rc < 0);
 }
 
